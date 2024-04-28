5a0e4438c71ae01a0fee7320bdf7a7344165f3d2
Mr-Bobert <116753769+Mr-Bobert@users.noreply.github.com>
2024-04-28 05:24:57 -0800

    Update LoginCheck.java

--- MakerHub JSP/MakerHub/WEB-INF/classes/makerhub/LoginCheck.java
+++ MakerHub JSP/MakerHub/WEB-INF/classes/makerhub/LoginCheck.java
@@ -17,4 +17,43 @@ public class LoginCheck extends DBConnect{
         }
         return false;
     }
+    
+    public Login getAccountType(String name,String passw) {
+        Login em = null;
+        String sql = "SELECT * FROM `Login` WHERE `Username` = ? AND  Password = ?";
+
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, name);
+            ps.setString(2, passw);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                em = new Login();
+                em.name=rs.getString("Username");
+                em.passw=rs.getString("Password");
+                em.account=rs.getString("Account_Type");
+               
+            }
+        } catch (SQLException e) {
+            System.out.println("ERROR - getAccountType()" + e.getMessage());
+        }
+
+        return em;
+    }
+      public boolean checkOwner(String username) {
+        try {
+            PreparedStatement pst = conn.prepareStatement("SELECT * FROM Space_Owner WHERE Name=?" );
+            pst.setString(1, username);
+           
+            ResultSet rs = pst.executeQuery();
+            if(rs.next()) {
+                return true;
+            }
+        } catch(SQLException e) {
+            System.out.println("SQL Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
